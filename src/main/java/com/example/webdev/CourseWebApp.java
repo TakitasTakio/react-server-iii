0d41b2da95c1c64ec8254a2006297143b9e3aff8
Takio W <oswini@Oswins-MacBook-Pro.local>
2018-06-27 16:10:34 -0400

    1st commit

---
+++ src/main/java/com/example/webdev/CourseWebApp.java
@@ -0,0 +1,12 @@
+package com.example.webdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWebApp.class, args);
+	}
+}
