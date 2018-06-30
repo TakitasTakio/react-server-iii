487295379d3b5bb61d4f3e498da421803d4bd91f
Takio W <oswini@Oswins-MacBook-Pro.local>
2018-06-30 19:43:28 -0400

    server revised

---
+++ src/main/java/com/example/webdev/repositories/ModuleRepository.java
@@ -0,0 +1,10 @@
+package com.example.webdev.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.webdev.models.Module;
+
+public interface ModuleRepository
+ extends CrudRepository<Module, Integer> {
+
+}
